f12168b1374cff7f71246993663689b14322f0f4
MohamedJABRANE <mohamed.jabrane.93@gmail.com>
2017-12-25 09:07:50 +0000

    ss

--- build/main.js
+++ build/main.js
@@ -331,6 +331,41 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = y[op[0] & 2 ? "return" : op[0] ? "throw" : "next"]) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [0, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
 
 
 
@@ -411,15 +446,30 @@ var Historical = (function () {
     }
     // initialisation du map + get current coordinate user
     Historical.prototype.ngOnInit = function () {
-        var _this = this;
         this.initMap();
         this.init();
-        this.geolocation.getCurrentPosition().then(function (resp) {
-            _this.currentlocationlat = resp.coords.latitude;
-            _this.currentlocationlng = resp.coords.longitude;
-            _this.coords = resp.coords;
-        }).catch(function (error) {
-            console.log('Error getting location', error);
+        // this.geolocation.getCurrentPosition().then((resp) => {
+        //   this.currentlocationlat=resp.coords.latitude;
+        //   this.currentlocationlng=resp.coords.longitude;
+        //   this.coords=resp.coords;
+        //  }).catch((error) => {
+        //    console.log('Error getting location', error);
+        //  });
+        this.getlocation();
+    };
+    Historical.prototype.getlocation = function () {
+        return __awaiter(this, void 0, void 0, function () {
+            var coords;
+            return __generator(this, function (_a) {
+                switch (_a.label) {
+                    case 0: return [4 /*yield*/, this.geolocation.getCurrentPosition()];
+                    case 1:
+                        coords = (_a.sent()).coords;
+                        this.currentlocationlat = coords.latitude;
+                        this.currentlocationlng = coords.longitude;
+                        return [2 /*return*/];
+                }
+            });
         });
     };
     // open menu
@@ -546,7 +596,7 @@ var Historical = (function () {
                     });
                 },
                 waypoints: [
-                    L.latLng(33.573110, -7.589843),
+                    L.latLng(this.currentlocationlat, this.currentlocationlng),
                     L.latLng(this.latpoi, this.lngpoi),
                 ],
                 language: 'fr',
